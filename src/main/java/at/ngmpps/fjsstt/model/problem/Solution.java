6952b7f7c75c467d31d7a814d731f4f0006302f2
gw <georg.weichhart@jku.at>
2018-09-26 16:19:12 +0200

    reverted long runtime instead of int iteration for marking the time of thesolution

--- src/main/java/at/ngmpps/fjsstt/model/problem/Solution.java
+++ src/main/java/at/ngmpps/fjsstt/model/problem/Solution.java
@@ -2,9 +2,8 @@ package at.ngmpps.fjsstt.model.problem;
 
 import java.io.Serializable;
 import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.HashMap;
+import java.util.Map;
 
 import at.ngmpps.fjsstt.model.problem.subproblem.Bid;
 
