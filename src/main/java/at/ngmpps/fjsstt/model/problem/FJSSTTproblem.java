ed5ca93ec46dcd8d632009602774aeb812494367
gw <georg.weichhart@jku.at>
2018-09-26 12:57:31 +0200

    imports organised

--- src/main/java/at/ngmpps/fjsstt/model/problem/FJSSTTproblem.java
+++ src/main/java/at/ngmpps/fjsstt/model/problem/FJSSTTproblem.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
-import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
